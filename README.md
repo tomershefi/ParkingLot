8d2251050b9c105b1d538ce53057a9e15a4afab5
tomer.shefi <tomer.shefi@moovit.com>
2024-06-03 19:06:34 +0300

    refactor 1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-execute deploy.sh file to run the program
+Steps:
+
+1. clone the repo - https://github.com/tomershefi/parkingLot.git
+2. execute deploy.sh file to run the program
--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,6 @@
 
 sudo yum install java-21-amazon-corretto
 
-git clone https://github.com/tomershefi/parkingLot.git
-
-cd parkingLot
-
 ./gradlew build
 
 java -jar build/libs/demo-0.0.1-SNAPSHOT.jar
